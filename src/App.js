0afff59e47f8898784f504e9b6ca6550910526f4
ljupcho <rljupcho@gmail.com>
2019-12-03 01:08:18 +0100

    draw the palettes and diagram

--- src/App.js
+++ src/App.js
@@ -1,89 +1,105 @@
-import React from 'react';
-import './App.css';
-import * as go from 'gojs';
-import { ReactDiagram, ReactPalette } from 'gojs-react';
+import React from "react";
 
-function initDiagram(diagramId) {
+import * as go from "gojs";
+import { ReactDiagram, ReactPalette } from "gojs-react";
+
+import "./App.css"; // contains .diagram-component CSS
+
+// ...
+
+function nodeClicked(e, obj) {
+  // executed by click and doubleclick handlers
+  var node = obj.part;
+  console.log(node.data.text);
+}
+/**
+ * This function is responsible for setting up the diagram's initial properties and any templates.
+ */
+function initDiagram() {
   const $ = go.GraphObject.make;
+  const diagram = $(go.Diagram, {
+    "undoManager.isEnabled": true, // enable undo & redo
+    "clickCreatingTool.archetypeNodeData": {
+      text: "new node",
+      color: "lightblue"
+    },
+    model: $(go.GraphLinksModel, {
+      linkKeyProperty: "key" // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
+    })
+  });
 
-    const myDiagram = $(go.Diagram, "root", {
-        initialContentAlignment: go.Spot.LeftCenter
-    });
+  // define a simple Node template
+  diagram.nodeTemplate = $(
+    go.Node,
+    "Auto", // the Shape will go around the TextBlock
+    { click: nodeClicked },
+    $(
+      go.Shape,
+      { width: 70, height: 20, fill: "white" },
+      new go.Binding("fill", "color")
+    ),
 
-    myDiagram.nodeTemplate = $(
-        go.Node,
-        'Auto',
-        $(go.Shape, 'RoundedRectangle', { strokeWidth: 0 }, new go.Binding('fill', 'color')),
-        $(go.TextBlock, { margin: 8 }, new go.Binding('text', 'key'))
-    );
+    $(go.TextBlock, { margin: 3 }, new go.Binding("text", "text"))
+  );
 
-    return myDiagram;
+  return diagram;
 }
 
 function initPalette() {
   const $ = go.GraphObject.make;
-  const palette =
-  $(go.Palette, "palette-content",
-	{
-		layout: $(go.GridLayout,
-		{
-			cellSize: new go.Size(200, 20),
-			wrappingColumn: 1
-		})      
-	});
-  palette.nodeTemplate = $(go.Node, "Horizontal", 
-  {    
-       movable: false
-  },
-    $(go.Shape,
-      { width: 15, height: 15, fill: "white" },
-      new go.Binding("fill", "color")),
-    $(go.TextBlock,
-      new go.Binding("text", "color"))
+  const palette = $(go.Palette, {
+    "undoManager.isEnabled": true, // enable undo & redo
+    "clickCreatingTool.archetypeNodeData": {
+      text: "new node",
+      color: "lightblue"
+    }
+  });
+
+  // define a simple Node template
+  palette.nodeTemplate = $(
+    go.Node,
+    "Vertical",
+    $(
+      go.Shape,
+      { width: 14, height: 14, fill: "white" },
+      new go.Binding("fill", "color")
+    ),
+    $(go.TextBlock, new go.Binding("text", "text"))
   );
+
   return palette;
 }
 
-
+/**
+ * This function handles any changes to the GoJS model.
+ * It is here that you would make any updates to your React state, which is dicussed below.
+ */
 function handleModelChange(changes) {
   console.log(changes);
 }
 
+// render function...
 function App() {
   return (
-    <div>
-      <ReactDiagram 
-        diagramId="myDiagramDiv"
-        initDiagram={initDiagram}
-        divClassName='diagram-component'
-        nodeDataArray={[
-          { key: 0, text: 'Alpha', color: 'lightblue', loc: '0 0' },
-          { key: 1, text: 'Beta', color: 'orange', loc: '150 0' },
-          { key: 2, text: 'Gamma', color: 'lightgreen', loc: '0 150' },
-          { key: 3, text: 'Delta', color: 'pink', loc: '150 150' }
-        ]}
-        linkDataArray={[
-          { key: -1, from: 0, to: 1 },
-          { key: -2, from: 0, to: 2 },
-          { key: -3, from: 1, to: 1 },
-          { key: -4, from: 2, to: 3 },
-          { key: -5, from: 3, to: 0 }
-        ]}
-        onModelChange={(e) => handleModelChange(e)}
-      />
-    {/* <div id="palette-content" style={{'width': '500px', 'height': '500px', 'backgroundColor': '#DAE4E4'}}>
+    <div class="App">
+      ...
       <ReactPalette
         initPalette={initPalette}
-        divClassName='palette-component'
+        divClassName="palette-component"
         nodeDataArray={[
-          { key: "LB", color: "lightblue" },
-          { key: "P", color: "pink" },
-          { key: "Y", color: "yellow" },
-          { key: "LG", color: "lightgreen" },
-          { key: "O", color: "orange" }
+          { key: 0, text: "Alpha", color: "lightblue", loc: "0 0" },
+          { key: 1, text: "Beta", color: "orange", loc: "150 0" },
+          { key: 2, text: "Gamma", color: "lightgreen", loc: "0 150" },
+          { key: 3, text: "Delta", color: "pink", loc: "150 150" }
         ]}
       />
-      </div> */}
+      <ReactDiagram
+        initDiagram={initDiagram}
+        divClassName="diagram-component"
+        onModelChange={e => handleModelChange(e)}
+      />
+      <div class="parameter-component"></div>
+      ...
     </div>
   );
 }
